c592ca03913e6ee4c368d72df7af7a7c4dd07dca
Godomcity <134502441+Godomcity@users.noreply.github.com>
2024-10-11 20:15:29 +0900

    CharacterAnimation
    캐릭터 애니메이션 추가

---
+++ Assets/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerAnimationController : AnimationController
+{
+    private static readonly int IsWalking = Animator.StringToHash("isWalking");
+
+    private readonly float magnituteThreshold = 0.5f;
+
+    protected void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        controller.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 vector)
+    {
+        animator.SetBool(IsWalking, vector.magnitude > magnituteThreshold);
+    }
+}
